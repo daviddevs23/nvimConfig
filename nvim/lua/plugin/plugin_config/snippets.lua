4169c2a30c23366369983ecff46950701858e577
David Hochstetler <daviddevs23@gmail.com>
2024-06-03 15:12:06 -0400

    added keymaps for luas

--- nvim/lua/plugin/plugin_config/snippets.lua
+++ nvim/lua/plugin/plugin_config/snippets.lua
@@ -2,7 +2,11 @@
 require("luasnip.loaders.from_vscode").lazy_load()
 
 -- Snippet setup
+local ls = require("luasnip")
 
+-- vim.keymap.set({ "i" }, "<C-K>", function() ls.expand() end, { silent = true })
+vim.keymap.set({ "i", "s" }, "<Tab>", function() ls.jump(1) end, { silent = true })
+vim.keymap.set({ "i", "s" }, "<C-Tab>", function() ls.jump(-1) end, { silent = true })
 
 -- Custom snippets
 -- Flask template example!!
