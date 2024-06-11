fbb6f01af359437e8effc59a3ec01a4af7484f27
David Hochstetler <daviddevs23@gmail.com>
2024-06-11 13:00:56 -0400

    resolved keybind conflixt

--- nvim/lua/plugin/plugin_config/snippets.lua
+++ nvim/lua/plugin/plugin_config/snippets.lua
@@ -5,7 +5,7 @@ require("luasnip.loaders.from_vscode").lazy_load()
 local ls = require("luasnip")
 
 -- vim.keymap.set({ "i" }, "<C-K>", function() ls.expand() end, { silent = true })
-vim.keymap.set({ "i", "s" }, "<Tab>", function() ls.jump(1) end, { silent = true })
+vim.keymap.set({ "i", "s" }, "<S-Tab>", function() ls.jump(1) end, { silent = true })
 vim.keymap.set({ "i", "s" }, "<C-Tab>", function() ls.jump(-1) end, { silent = true })
 
 -- Custom snippets
