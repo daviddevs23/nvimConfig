c8b3fce5ccf5eb2645313a32c709a7981d4a4545
David Hochstetler <daviddevs23@gmail.com>
2026-04-20 09:54:06 -0400

    Cleaned up lsp

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -115,17 +115,5 @@ return {
     },
     {
         "nvim-treesitter/nvim-treesitter",
-
-        -- config = function()
-        --     require("nvim-treesitter.configs").setup {
-        --         ensure_installed = { "cpp", "c", "go", "bash", "javascript", "typescript", "lua", "rust", "python" },
-        --         sync_install = false,
-        --         auto_install = true,
-        --         highlight = {
-        --             enable = true,
-        --             additional_vim_regex_highlighting = false,
-        --         },
-        --     }
-        -- end
     }
 }
