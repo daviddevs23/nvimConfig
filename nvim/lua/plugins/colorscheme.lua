0e979dfbd63c3cf6bbf520e19144fd92527e242f
David Hochstetler <daviddevs23@gmail.com>
2026-06-01 16:11:14 -0400

    Replaced deprecated colorizer

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,33 +1,33 @@
 return {
-    {
-        'ellisonleao/gruvbox.nvim',
-        priority = 1000,
-        lazy = false,
-        config = function()
-            vim.cmd.colorscheme 'gruvbox'
-        end,
-    },
-    {
-        "j-hui/fidget.nvim",
-        opts = {},
-    },
-    {
-        "norcalli/nvim-colorizer.lua",
-        config = function()
-            require("colorizer").setup({
-                "*",
-                user_default_options = {
-                    RGB = true,
-                    RRGGBB = true,
-                    names = true,
-                    RRGGBBAA = true,
-                    AARRGGBB = false,
-                    rgb_fn = true,
-                    hsl_fn = true,
-                    css = true,
-                    css_fn = true,
-                },
-            })
-        end,
-    }
+  {
+    'ellisonleao/gruvbox.nvim',
+    priority = 1000,
+    lazy = false,
+    config = function()
+      vim.cmd.colorscheme 'gruvbox'
+    end,
+  },
+  {
+    "j-hui/fidget.nvim",
+    opts = {},
+  },
+  {
+    "catgoose/nvim-colorizer.lua",
+    config = function()
+      require("colorizer").setup({
+        "*",
+        user_default_options = {
+          RGB = true,
+          RRGGBB = true,
+          names = true,
+          RRGGBBAA = true,
+          AARRGGBB = false,
+          rgb_fn = true,
+          hsl_fn = true,
+          css = true,
+          css_fn = true,
+        },
+      })
+    end,
+  }
 }
